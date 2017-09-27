ae63e0899fe225c9c654e69bc9a8f4f9293d6990
Kyle Ruschel <dkr1985@icloud.com>
2017-09-26 21:30:56 -0400

    added some commented out code that we could incorporate

--- assets/javascript/meme.js
+++ assets/javascript/meme.js
@@ -1,6 +1,21 @@
 //Created on click function that starts on the click
 $('#').on('click', function() {
 
+  /*
+  this is an example to spling the quote in half I found from StackOverflow,
+  if we have time we may try to incorporate this into the meme, if not, we can just dump the entire
+  quote into the text1 variable and the memegen api will auto-size the string to fit correctly
+  inside the image without any parameters.
+
+  You can visit this link to read and understand the code and how it works
+  https://stackoverflow.com/questions/18087416/split-string-in-half-by-word
+
+  var s = "This is a long string";
+  var middle = Math.floor(s.length / 2);
+  var before = s.lastIndexOf(' ', middle);
+  var after = s.indexOf(' ', middle + 1);
+  */
+
   //Created a variable to hold the "api" url
   var url = "#";
   //Created a variable to hold the top text of the meme

14cf937c1c2ca47630a021f5fe442e6645d99aac
Kyle Ruschel <dkr1985@icloud.com>
2017-09-26 21:06:17 -0400

    added ajax call and function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,12 +8,17 @@
 	//onclick for joke generation. From joke btn
 		//calls function from chuck.js file places response into var jokeData
 		//target and display joke text on index.html element
+		$('#').on('click' function() {
+			jokeData = chuckie();
+ 		$('#').innerhtml(jokeData);
+});
+
 
 /////IMGUR SECTION
 	//onclick for image search. Uses imput from text field and search btn event
 		//calls function from imgur.js file places response into var imageUrl
 		imageUrl = getImage(imageSearch);
-		//target and display image in index.html crousel element. Pass in the 
+		//target and display image in index.html crousel element. Pass in the
 		//imageSerarch value as a paramenter for the function in the imgur.js file.
 
 		//how are we going to select the image to use????
@@ -32,5 +37,5 @@
 
 /////TEXT2SPEECH
 	//timer count down 2000 ms and call function in text2speech.js file
-		//if we have time pass the var jokeData as a parameter for the text2Speech 
-		//function in the text2speech.js file. 
+		//if we have time pass the var jokeData as a parameter for the text2Speech
+		//function in the text2speech.js file.
